834228bf0d2c873a9e9496b0a0889e90009c5bb9
star <star@qq.com>
2021-04-07 14:06:18 +0800

    修改启动无法打开页面的问题

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,6 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: 'development',
+  NODE_ENV: '"development"',
   BASE_API: 'https://mock.mengxuegu.com/mock/6035fa1eca363222f3d579e6/example',
 })
